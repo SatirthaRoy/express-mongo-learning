1f369a535ccb0b696ffe0884093b33cd8fbc0fb7
himu <satirtharoy2003@gmail.com>
2024-04-20 01:54:25 +0600

    crud learnt

--- index.js
+++ index.js
@@ -38,6 +38,60 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+
+    const database = client.db("userDB");
+    const user = database.collection("user");
+
+    app.get('/', (req, res)=> {
+      res.send('onlymagi');
+    })
+    
+    app.get('/users', async(req, res) => {
+      const cursor = user.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get('/update/:name', async(req, res) => {
+      const name = req.params.name;
+      const query = {name: name}
+      const suser = await user.findOne(query);
+      console.log(suser);
+      res.send(suser);
+    })
+
+    app.post('/users', async(req, res) => {
+      console.log('ouch');
+      console.log(req.body);
+      users.push(req.body);
+      
+      const resutlt = await user.insertOne(req.body);
+      res.send(resutlt);
+    })
+
+    app.put('/update/:name', async(req, res) => {
+      const name = req.params.name;
+      const updatedUser = req.body;
+      console.log(updatedUser, name);
+      const filter = {name: name};
+      const option = {upsert : true};
+      const updatedDoc = {
+        $set: {
+          name: updatedUser.name,
+          phone: updatedUser.phone
+        }
+      }
+      const result = await user.updateOne(filter, updatedDoc, option);
+      res.send(result);
+    })
+
+    app.delete('/users', async(req, res) => {
+      const query = { name: req.body.name };
+      const result = await user.deleteOne(query);
+      res.send(result)
+    })
+
+
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
@@ -52,13 +106,13 @@ run().catch(console.dir);
 
 
 
-app.get('/', (req, res)=> {
-  res.send('onlymagi');
-})
+// app.get('/', (req, res)=> {
+//   res.send('onlymagi');
+// })
 
-app.get('/users', (req, res) => {
-  res.send(users);
-})
+// app.get('/users', (req, res) => {
+//   res.send(users);
+// })
 
 app.post('/users', (req, res) => {
   console.log('ouch');
